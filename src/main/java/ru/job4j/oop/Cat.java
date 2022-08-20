729255368bc3ab0230d5baa3ee649c62968eb9f4
Evgeny Lim <lim.evny@gmail.com>
2022-08-21 01:47:44 +0900

    1.1. Создание объекта [#165452 #169487]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,6 +1,15 @@
 package ru.job4j.oop;
 
 public class Cat {
-    Cat peppy = new Cat();
-    Cat sparky = new Cat();
+
+    public String sound() {
+        String voice = "May-may";
+        return voice;
+    }
+
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says: " + say);
+    }
 }
