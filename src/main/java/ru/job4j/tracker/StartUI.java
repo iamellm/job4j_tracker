13e8c0dc31cc85b78512eb65e264e58d84ab6179
Evgeny Lim <lim.evny@gmail.com>
2022-09-01 03:23:34 +0900

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,6 +8,8 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTime = item.getCreated().format(formatter);
         System.out.println("Текущие дата, день и время: "  + currentDateTime);
+        System.out.println();
+        System.out.println(item);
     }
 
 }
