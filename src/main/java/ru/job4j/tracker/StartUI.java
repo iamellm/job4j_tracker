4dd6d316b321b6975ef301c73ff2727fc14647d9
Evgeny Lim <lim.evny@gmail.com>
2022-09-11 01:35:02 +0900

    Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -108,5 +108,6 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         new StartUI().init(input, tracker);
+        StartUI.createItem(input, tracker);
     }
 }
